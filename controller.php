31ca90018afccf661d3f28db8f0ffd79aacb0d38
Kadeem Hassam <kadeem@gmail.com>
2012-11-12 10:46:42 -0500

    Removed PHP execution time on script for large data loads.

--- controller.php
+++ controller.php
@@ -1,4 +1,5 @@
 <?php
+set_time_limit(0);
 function getGuid() {
   return loadDoc(doCurl("https://www.pivotaltracker.com/services/v3/tokens/active", "POST", array('username'=>$_POST['uname'], 'password'=>$_POST['pword'])))->getElementsByTagName('guid')->item(0)->nodeValue;
 }
